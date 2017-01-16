029095765f0ab95f8dc2e8b715494acac4509fcc
github username <github email address>
2017-01-16 14:58:53 -0500

    init commit

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,13 @@
 def find_closing_parenthesis(sentence, open_position)
-
+  char_arr = sentence.split('')
+  interior_count = 0
+  final_idx = nil
+  while(final_idx == nil)
+    open_position += 1
+    current_char = char_arr[open_position]
+    final_idx = open_position if current_char == ")" && interior_count == 0
+    interior_count -= 1 if current_char == ")"
+    interior_count += 1 if current_char == "("
+  end
+  final_idx
 end
